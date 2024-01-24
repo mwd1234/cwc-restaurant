aebc3297b73557aa05dc462e418165a4950508a0
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-24 16:52:10 -0700

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Matthew Dembinski on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
